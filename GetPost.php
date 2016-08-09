8f6bf21f0ba22f270fa3d9ff59c3d8642583e524
chawit <chawitmild@gmail.com>
2016-08-09 14:11:45 +0700

    Update GetPost.php

--- GetPost.php
+++ GetPost.php
@@ -3,41 +3,35 @@
 Copyright (c) 2016 Chawit Wiriyakun
 */
 Class GetPost{
-	
 public function ConvertALLKeyToLOWER(){
 	$_GET = array_change_key_case($_GET, CASE_LOWER);
 	$_POST = array_change_key_case($_POST, CASE_LOWER);
-	
 }
 public function ConvertALLKeyToUPPER(){
 	$_GET = array_change_key_case($_GET, CASE_UPPER);
 	$_POST = array_change_key_case($_POST, CASE_UPPER);
-	
 }
 public function ConvertManual($GETWant,$POSTWant){
 	$GETWant=strtolower($GETWant);
 	$POSTWant=strtolower($POSTWant);
 	if($GETWant=="upper"){
 	$_GET = array_change_key_case($_GET, CASE_UPPER);	
-		
 	}else{
 	$_GET = array_change_key_case($_GET, CASE_LOWER);				
 	}
 	if($POSTWant=="lower"){
 	$_POST = array_change_key_case($_POST, CASE_LOWER);	
-		
 	}else{
 	$_POST = array_change_key_case($_POST, CASE_UPPER);				
 	}
-	
 }
-
 public static function SearchGET($key)
 {
 	$key=strtolower($key);
   	$GETVault=$_GET;
 	$GETVault = array_change_key_case($GETVault, CASE_LOWER);	
-    if(isset($GETVault[$key])){
+    if(isset($GETVault[$key]))
+        {
 		return $GETVault[$key];
 	}
 	else
@@ -45,7 +39,6 @@ public static function SearchGET($key)
 		return "";
 	}		
 }
-
 public  static function SearchPOST($key)
 {
 	$key=strtolower($key);
@@ -59,9 +52,5 @@ public  static function SearchPOST($key)
 		return "";
 	}		
 }
-
-   
 }
-
-
 ?>
